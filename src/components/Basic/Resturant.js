1ea570cf1d71b445319adab1712a49c2f78ed329
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-08-03 03:27:13 +0530

    helix-resturant-app

---
+++ src/components/Basic/Resturant.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import './style.css'
+import Menu from "./menuApi.js";
+import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList  = [...new Set(Menu.map((curElem)=>{
+    return curElem.category;
+})),
+"All",];
+
+const Resturant = () => {
+
+    const [MenuData , setMenuData] = useState(Menu);
+    const [menuList , setMenuList] = useState(uniqueList);
+
+    const filterItem  = (category) => {
+        if(category === "All"){
+            setMenuData(Menu);
+            return;
+        }
+        const updatedList = Menu.filter((curElem)=>{
+            return curElem.category === category;
+        });
+        setMenuData(updatedList);
+    }
+  return (
+    <>
+    <Navbar filterItem = {filterItem} menuList = {menuList}/>
+    <MenuCard MenuData={MenuData}/>
+    </>
+  )
+}
+
+export default Resturant
